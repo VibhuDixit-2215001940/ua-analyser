3ca7f129b34afeb6c1b581a05371489f72161be0
Vibhu Dixit <vibhu.dixit@onmeridian.com>
2025-10-16 15:07:14 +0530

    add brave bowser ua

--- public/script.js
+++ public/script.js
@@ -1,10 +1,15 @@
 async function getInfo(){
-  const res = await fetch(window.location.origin + '/api/info')
+  const res = await fetch('/api/info')
   const data = await res.json()
+  let brave = false
+  if (navigator.brave) brave = await navigator.brave.isBrave()
+  
+  let browserName = brave ? "Brave (Chromium Base)" : data.browser
+
   let html = `
     <table>
       <tr><td><b>IP Address</b></td><td>${data.ip}</td></tr>
-      <tr><td><b>Browser</b></td><td>${data.browser}</td></tr>
+      <tr><td><b>Browser</b></td><td>${browserName}</td></tr>
       <tr><td><b>OS</b></td><td>${data.os}</td></tr>
       <tr><td><b>Device Type</b></td><td>${data.device}</td></tr>
       <tr><td><b>Location</b></td><td>${data.city}, ${data.country}</td></tr>
